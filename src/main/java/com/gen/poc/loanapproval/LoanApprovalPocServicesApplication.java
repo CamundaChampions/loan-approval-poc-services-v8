3a2262eb55b12a6656ff5b9643054a965db34bce
Ravish Raj <ravish4public@gmail.com>
2024-10-11 16:25:06 +0530

    POC: Loan CORS ALLOW

--- src/main/java/com/gen/poc/loanapproval/LoanApprovalPocServicesApplication.java
+++ src/main/java/com/gen/poc/loanapproval/LoanApprovalPocServicesApplication.java
@@ -3,6 +3,10 @@ package com.gen.poc.loanapproval;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Deployment(resources = "classpath:LOAN_APPROVAL_PROCESS.bpmn")
 @SpringBootApplication
@@ -12,4 +16,16 @@ public class LoanApprovalPocServicesApplication {
 		SpringApplication.run(LoanApprovalPocServicesApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowedOrigins("*");
+			}
+		};
+	}
 }
